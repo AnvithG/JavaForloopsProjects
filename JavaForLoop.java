00623b8034ad85e21f9ebb330d03da42d3560e20
Anvith Gudem <73951382+AnvithG@users.noreply.github.com>
2022-04-04 19:44:52 +0530

    Add files via upload

---
+++ JavaForLoop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class JavaForLoop {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to see it's multiplication table...");
+        n = sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(n + " x " + i + " = "  + i * n);
+        }
+    }
+}
